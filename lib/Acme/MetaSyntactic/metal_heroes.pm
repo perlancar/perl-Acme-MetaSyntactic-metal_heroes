224f8586f9668e36fe519c8c2a1756d76713b6da
perlancar (on pc-home) <perlancar@gmail.com>
2020-03-01 09:24:06 +0700

    Initial commit

---
+++ lib/Acme/MetaSyntactic/metal_heroes.pm
@@ -0,0 +1,44 @@
+package Acme::MetaSyntactic::metal_heroes;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use parent qw(Acme::MetaSyntactic::MultiList);
+__PACKAGE__->init;
+
+1;
+# ABSTRACT: The Metal Heroes series theme
+
+=head1 SYNOPSIS
+
+ % perl -MAcme::MetaSyntactic=metal_heroes -le 'print metaname'
+ gavan
+
+ % metasyn metal_heroes | shuf | head -n2
+ juspion
+ shaider
+
+
+=head1 DESCRIPTION
+
+TODO: enemies.
+
+
+=head1 SEE ALSO
+
+L<https://en.wikipedia.org/wiki/Metal_Hero_Series>
+
+L<Acme::MetaSyntactic::gavan>
+
+L<Acme::MetaSyntactic>
+
+=cut
+
+__DATA__
+# default
+heroes
+# names heroes
+gavan sharivan shaider juspion spielban metalder jiraiya jiban winspector solbrain exceedraft janperson blueswat bfighter kabuto kabutack robotack
+# names enemies
